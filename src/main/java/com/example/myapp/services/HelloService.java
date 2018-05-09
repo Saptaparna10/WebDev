03ce59c97c422735f3de342995fe1ac503a1cac2
Saptaparna10 <saptaparna1991@gmail.com>
2018-05-08 20:57:54 -0400

    database creation

---
+++ src/main/java/com/example/myapp/services/HelloService.java
@@ -0,0 +1,17 @@
+package com.example.myapp.services;
+
+import com.example.myapp.model.Hello;
+import com.example.myapp.repositories.HelloRepository;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloService {
+	@Autowired
+	HelloRepository repository;
+	@GetMapping("/api/hello")
+	public Iterable<Hello> findAllHellos() {
+		return repository.findAll();
+	}
+}
