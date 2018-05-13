67b976dcb28814c8ee7431231b68009291a89a67
Saptaparna10 <saptaparna1991@gmail.com>
2018-05-13 13:35:49 -0400

    CRUD

--- src/main/java/com/example/webdevsummer12018/services/HelloService.java
+++ src/main/java/com/example/webdevsummer12018/services/HelloService.java
@@ -1,17 +1,20 @@
-package com.example.myapp.services;
+package com.example.webdevsummer12018.services;
 
-import com.example.myapp.model.Hello;
-import com.example.myapp.repositories.HelloRepository;
-import org.springframework.beans.factory.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.webdevsummer12018.models.Hello;
+import com.example.webdevsummer12018.repositories.HelloRepository;
 
 @RestController
 public class HelloService {
 	@Autowired
 	HelloRepository repository;
+	
 	@GetMapping("/api/hello")
 	public Iterable<Hello> findAllHellos() {
 		return repository.findAll();
 	}
+
 }
--- src/test/java/com/example/webdevsummer12018/DemoApplicationTests.java
+++ src/test/java/com/example/webdevsummer12018/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.myapp;
+package com.example.webdevsummer12018;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
